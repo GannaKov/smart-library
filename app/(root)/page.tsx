f9489e1696e5aef5c39823937651ef218039a068
GannaKov <zlatta2000@gmail.com>
2025-03-07 18:32:04 +0100

    add header, (root)

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button';
+
+const Home = () => {
+  return (
+    <>
+      <h1 className="px-4 py-2 text-chart-4 sm:px-8">Home</h1>
+      <Button>Click</Button>
+    </>
+  );
+};
+
+export default Home;
