09dac8025099a5c72ed8954b4a963e281b4442ed
GannaKov <zlatta2000@gmail.com>
2025-03-07 18:54:10 +0100

    add fake data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,12 @@
+import BookList from '@/components/BookList';
+import BookOverview from '@/components/BookOverview';
 import { Button } from '@/components/ui/button';
 
 const Home = () => {
   return (
     <>
-      <h1 className="px-4 py-2 text-chart-4 sm:px-8">Home</h1>
-      <Button>Click</Button>
+      <BookOverview />
+      <BookList />
     </>
   );
 };
