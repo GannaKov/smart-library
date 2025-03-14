63514a1717e0d45e0a5b4babaaabb05e846fe4fe
GannaKov <zlatta2000@gmail.com>
2025-03-14 18:43:18 +0100

    book-card

--- components/BookList.tsx
+++ components/BookList.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import BookCard from './BookCard';
 
 interface Props {
   title: string;
@@ -10,6 +11,11 @@ const BookList = ({ title, books, containerClassName }: Props) => {
   return (
     <section className={containerClassName}>
       <h2 className="font-bebas-neue text-4xl text-light-100">{title}</h2>
+      <ul className="book-list">
+        {books.map((book) => (
+          <BookCard key={book.title} {...book} />
+        ))}
+      </ul>
     </section>
   );
 };
