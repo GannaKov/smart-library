955e5b88c5f80eab38aecb200a9b295f7d3f3c16
GannaKov <zlatta2000@gmail.com>
2025-03-28 21:33:01 +0100

    validation, start form

---
+++ components/AuthForm.tsx
@@ -0,0 +1,38 @@
+'use client';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  DefaultValues,
+  FieldValues,
+  Path,
+  SubmitHandler,
+  useForm,
+  UseFormReturn,
+} from 'react-hook-form';
+import { ZodType } from 'zod';
+
+import { Button } from '@/components/ui/button';
+
+interface Props<T extends FieldValues> {
+  schema: ZodType<T>;
+  defaultValues: T;
+  onSubmit: (data: T) => Promise<{ success: boolean; error?: string }>;
+  type: 'SIGN_IN' | 'SIGN_UP';
+}
+
+const AuthForm = <T extends FieldValues>({
+  type,
+  schema,
+  defaultValues,
+  onSubmit,
+}: Props<T>) => {
+  const form: UseFormReturn<T> = useForm<T>({
+    resolver: zodResolver(schema),
+    defaultValues: defaultValues as DefaultValues<T>,
+  });
+
+  const handleSubmit: SubmitHandler<T> = async (data) => {};
+
+  return <div>SignForm--{type}</div>;
+};
+
+export default AuthForm;
